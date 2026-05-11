4f232ce7796f6a33f3b01b5bf6039dc3ce938069
Ирина <irishaniro@yandex.ru>
2026-05-12 02:25:15 +0300

    add all

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const DATA_FILE_PATH = path.join(__dirname, 'data', 'stocks.json');
 stocksService.init(DATA_FILE_PATH);
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
